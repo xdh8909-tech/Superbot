f34578fb35842451d7c6c4849eee7663dc7de555
xdh8909-tech <xdh8909@gmail.com>
2026-08-08 14:54:24 -0500

    Create wls.js

---
+++ src/commands/Welcome/wls.js
@@ -0,0 +1,72 @@
+import { SlashCommandBuilder, PermissionFlagsBits } from 'discord.js';
+import { logger } from '../../utils/logger.js';
+import { handleInteractionError, TitanBotError, ErrorTypes, replyUserError } from '../../utils/errorHandler.js';
+import embedCreateModule from './modules/embed_create.js';
+
+export default {
+    slashOnly: true,
+    data: new SlashCommandBuilder()
+        .setName('embed')
+        .setDescription('Crea y gestiona tarjetas embed personalizadas')
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('create')
+                .setDescription('Crea o actualiza una tarjeta embed con un nombre único')
+                .addStringOption(option =>
+                    option.setName('name')
+                        .setDescription('Nombre identificador para la tarjeta (ej. bienvenida1)')
+                        .setRequired(true))
+                .addStringOption(option =>
+                    option.setName('title')
+                        .setDescription('Título del embed (Soporta {username}, {server})')
+                        .setRequired(false))
+                .addStringOption(option =>
+                    option.setName('description')
+                        .setDescription('Texto principal ({user}, {username}, {server}, {memberCount})')
+                        .setRequired(false))
+                .addStringOption(option =>
+                    option.setName('color')
+                        .setDescription('Código Hex (#d9510c) o preset (primary, success, blurple)')
+                        .setRequired(false))
+                .addStringOption(option =>
+                    option.setName('image')
+                        .setDescription('URL del banner grande inferior')
+                        .setRequired(false))
+                .addStringOption(option =>
+                    option.setName('thumbnail')
+                        .setDescription('URL de la miniatura o escribe "user" para la foto del usuario')
+                        .setRequired(false))
+                .addStringOption(option =>
+                    option.setName('footer')
+                        .setDescription('Texto de pie de página')
+                        .setRequired(false))),
+
+    async execute(interaction, config, client) {
+        try {
+            if (!interaction.memberPermissions?.has(PermissionFlagsBits.ManageGuild)) {
+                return await replyUserError(interaction, { 
+                    type: ErrorTypes.PERMISSION, 
+                    message: 'Necesitas el permiso de **Administrar Servidor** para usar `/embed`.' 
+                });
+            }
+
+            const subcommand = interaction.options.getSubcommand();
+
+            switch (subcommand) {
+                case 'create':
+                    return await embedCreateModule.execute(interaction, config, client);
+                default:
+                    logger.warn(`Subcomando desconocido en /embed: ${subcommand}`);
+            }
+        } catch (error) {
+            if (error instanceof TitanBotError) {
+                return await replyUserError(interaction, { 
+                    type: ErrorTypes.CONFIGURATION, 
+                    message: error.userMessage || 'Ocurrió un error al procesar el comando.' 
+                });
+            }
+            await handleInteractionError(interaction, error, { command: 'embed' });
+        }
+    },
+};
