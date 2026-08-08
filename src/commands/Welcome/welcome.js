169d54f4b1f480a0e1557398a5d0032824fb60ee
xdh8909-tech <xdh8909@gmail.com>
2026-08-08 01:05:28 -0500

    Update welcome.js

--- src/commands/Welcome/welcome.js
+++ src/commands/Welcome/welcome.js
@@ -1,18 +1,18 @@
 import { SlashCommandBuilder, PermissionFlagsBits, ChannelType } from 'discord.js';
 import { logger } from '../../utils/logger.js';
 import { handleInteractionError, TitanBotError, ErrorTypes, replyUserError } from '../../utils/errorHandler.js';
-import welcomeSetup from './modules/welcome_setup.js';
+import wlcSetup from './modules/wlc_setup.js';
 
 export default {
     slashOnly: true,
     data: new SlashCommandBuilder()
-        .setName('welcome')
-        .setDescription('Configure and create custom embed welcome messages')
+        .setName('wlc')
+        .setDescription('Configure and customize the welcome system')
         .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
         .addSubcommand(subcommand =>
             subcommand
                 .setName('setup')
-                .setDescription('Set up a fully customizable welcome embed')
+                .setDescription('Configure a fully customizable welcome embed')
                 .addChannelOption(option =>
                     option.setName('channel')
                         .setDescription('The channel to send welcome messages to')
@@ -20,27 +20,27 @@ export default {
                         .setRequired(true))
                 .addStringOption(option =>
                     option.setName('title')
-                        .setDescription('Title for the welcome embed')
+                        .setDescription('Embed title ({username}, {server}, etc.)')
                         .setRequired(true))
                 .addStringOption(option =>
                     option.setName('message')
-                        .setDescription('Welcome text ({user}, {username}, {server}, {memberCount})')
+                        .setDescription('Welcome message ({user}, {username}, {server}, {memberCount})')
                         .setRequired(true))
                 .addStringOption(option =>
                     option.setName('color')
-                        .setDescription('Hex color code (e.g. #d9510c)')
+                        .setDescription('Hex code or color name (e.g. #d9510c, primary, blurple)')
                         .setRequired(false))
                 .addStringOption(option =>
                     option.setName('image')
-                        .setDescription('URL of the large banner image')
+                        .setDescription('URL of the large banner image at the bottom')
                         .setRequired(false))
                 .addStringOption(option =>
                     option.setName('thumbnail')
-                        .setDescription('URL for small image or write "user" for member avatar')
+                        .setDescription('URL for small side image or type "user" for member avatar')
                         .setRequired(false))
                 .addStringOption(option =>
                     option.setName('footer')
-                        .setDescription('Footer text at the bottom')
+                        .setDescription('Small text shown at the bottom of the embed')
                         .setRequired(false))
                 .addBooleanOption(option =>
                     option.setName('ping')
@@ -50,22 +50,28 @@ export default {
     async execute(interaction, config, client) {
         try {
             if (!interaction.memberPermissions?.has(PermissionFlagsBits.ManageGuild)) {
-                return await replyUserError(interaction, { type: ErrorTypes.PERMISSION, message: 'You need the **Manage Server** permission to use `/welcome`.' });
+                return await replyUserError(interaction, { 
+                    type: ErrorTypes.PERMISSION, 
+                    message: 'You need the **Manage Server** permission to use `/wlc`.' 
+                });
             }
 
             const subcommand = interaction.options.getSubcommand();
 
             switch (subcommand) {
                 case 'setup':
-                    return await welcomeSetup.execute(interaction, config, client);
+                    return await wlcSetup.execute(interaction, config, client);
                 default:
-                    logger.warn(`Unknown /welcome subcommand: ${subcommand}`);
+                    logger.warn(`Unknown /wlc subcommand: ${subcommand}`);
             }
         } catch (error) {
             if (error instanceof TitanBotError) {
-                return await replyUserError(interaction, { type: ErrorTypes.CONFIGURATION, message: error.userMessage || 'Something went wrong.' });
+                return await replyUserError(interaction, { 
+                    type: ErrorTypes.CONFIGURATION, 
+                    message: error.userMessage || 'Something went wrong.' 
+                });
             }
-            await handleInteractionError(interaction, error, { command: 'welcome' });
+            await handleInteractionError(interaction, error, { command: 'wlc' });
         }
     },
 };
